f1b2ab27884ac344cb8ce1cb9c1db942306a6b1c
黃皓正 <huanghaozheng@huanghaozhengdeMacBook-Pro.local>
2020-08-26 17:40:56 +0800

    8/26

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableWithoutFeedback, Keyboard} from 'react-native';
+
+import Header from './components/Header'
+import GameScreen from './screen/GameScreen'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback style={styles.container} onPress={() => {Keyboard.dismiss()}}>
+      <View>
+        <Header title="Guess a number"/>
+        <GameScreen />
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  }
 });
